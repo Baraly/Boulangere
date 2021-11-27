ec21587dcb00223a4b15c82ab20aef00206f507c
Baraly <baptiste.bronsin@etu.umontpellier.fr>
2021-11-27 06:37:28 +0100

    système d'inscription

--- code/compte.php
+++ code/compte.php
@@ -9,28 +9,67 @@
 </head>
 <body>
     <?php include_once("nav/nav.php"); ?>
-    <h2>Mon compte</h2>
-    <div class="row account">
-        <div class="col-sm-5 centre">
-            <h4 class="top">Connexion</h4>
-            <form action="comptePost.php?connexion" method="post">
-                <label>Email : <input type="email" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"/></label><br>
-                <label>Password : <input type="password" name="password"/></label>
-                <input type="submit" class="cache"/>
-            </form>
-        </div>
-        <div class="col-sm-2">
-            <span class="separation"></span>
-        </div>
-        <div class="col-sm-5 centre">
-            <h4 class="top">Inscription</h4>
-            <form action="comptePost.php?inscription" method="post">
-                <label>Nom : <input type="text" name="firstname"/></label><br>
-                <label>Prénom : <input type="text" name="surname"/></label><br>
-                <label>Email : <input type="email" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"/></label>
-                <input type="submit" class="cache"/>
-            </form>
-        </div>
-    </div>
+
+    <?php
+    $error = "";
+        if(isset($_GET['error']) && $_GET['error'] == "password")
+            $error = "<h3>Veuilliez vérifier que les deux mots de passes soient bien les mêmes</h3>";
+        if(isset($_GET['error']) && $_GET['error'] == "email")
+            $error = "<h3>Cette adresse email est déjà utilisée</h3>";
+
+        if(isset($_GET['inscription'])){
+            include_once("bdd.php");
+            $request = $bdd->query("SELECT email FROM Clients");
+            while($donnees = $request->fetch()){
+                if($_POST['email'] == $donnees['email'])
+                    header('location: compte.php?error=email');
+            }
+            if($error == ""){
+                ?>
+                <h2>Création de compte</h2>
+                <div class="account oneForm">
+                    <?php $form = "comptePost.php?nom=".$_POST['firstname']."&prenom=".$_POST['surname']."&email=".$_POST['email'] ?>
+                    <form action="<?= $form ?>" method="POST">
+                        <label>Créer un mot  de passe : <input type="password" name="password1"></label><br>
+                        <label>Confirmer le mot de passe : <input type="password" name="password2"></label>
+                        <input type="submit" class="cache">
+                    </form>
+                </div>
+                <center><?= $error ?></center>
+                <?php
+            }
+        }
+        else{
+            ?>
+            <h2>Mon compte</h2>
+            <div class="row account">
+                <div class="col-sm-5 centre">
+                    <h4 class="top">Connexion</h4>
+                    <form action="comptePost.php?connexion" method="post">
+                        <label>Email : <input type="email" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required/></label><br>
+                        <label>Password : <input type="password" name="password" required/></label>
+                        <input type="submit" class="cache"/>
+                    </form>
+                </div>
+                <div class="col-sm-2">
+                    <span class="separation"></span>
+                </div>
+                <div class="col-sm-5 centre">
+                    <h4 class="top">Inscription</h4>
+                    <form action="compte.php?inscription" method="post">
+                        <label>Nom : <input type="text" name="firstname" required/></label><br>
+                        <label>Prénom : <input type="text" name="surname" required/></label><br>
+                        <label>Email : <input type="email" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required/></label>
+                        <input type="submit" class="cache"/>
+                    </form>
+                </div>
+            </div>
+            <center><?= $error?></center>
+            <?php
+        }
+
+    ?>
+
+
 </body>
 </html>
--- code/comptePost.php
+++ code/comptePost.php
@@ -4,13 +4,42 @@
 
     }
 
-    if(isset($_GET['inscription'])){
-        // vérifier que l'email n'est pas déjà utilisé
-        ?>
-        <h4>À fin de vous proposer nos services de livraison, pouvez-vous nous communiquer votre adresse ? </h4>
+    // Lors de l'inscription
+    if(isset($_GET['email'])){
 
-        <h4></h4>
-        <?php
+        // Cas où les deux mots de passes ne soient pas les mêmes
+        if($_POST['password1'] != $_POST['password2'])
+            header("location: compte.php?error=password");
+
+        include_once("bdd.php");
+
+        $insert = $bdd->prepare("INSERT INTO Clients(nom, prenom, email, motDePasse, vkey) VALUES(:nom, :prenom, :email, :motDePasse, :vkey)");
+
+        // On 'crypte' le mot de passe afin de le sauvegarder dans la BDD
+        $password = password_hash($_POST['password1'], PASSWORD_DEFAULT);
+
+        // On génère une clé aleatoire
+        $verifiedkey = md5(time());
+
+        // On insère les données das la BDD
+        $insert->execute(array(
+            'nom' => $_GET['nom'],
+            'prenom' => $_GET['prenom'],
+            'email' => $_GET['email'],
+            'motDePasse' => $password,
+            'vkey' => $verifiedkey));
+
+        $to =  $_GET['email'];
+        $subject = "Email de vérification";
+        $message = "<a href='http://localhost:8888/ProjetFac/code/verify.php?vkey=$verifiedkey'>Valider mon compte</a>";
+        $headers = "From: baptiste.bronsin@outlook.com\r\n";
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-type: text/html; charset=UTF-8\r\n";
+
+        if(mail($to, $subject, $message))
+            echo "<h3>L'email a bien été envoyé !</h3>";
+        else
+            echo "<h3>L'email n'a pas été envoyé !</h3>";
     }
 
 ?>
