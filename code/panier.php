c7b34b810502f95961c73f3e1c4a68914c3daa44
Baraly <baptiste.bronsin@etu.umontpellier.fr>
2021-12-08 21:13:18 +0100

    paypal v1

--- code/panier.php
+++ code/panier.php
@@ -25,10 +25,65 @@ if(isset($_GET['suppProd'])){
     if(!isset($_SESSION['email'])){
         echo "<center style='margin-top: 50px'><h4>Veuillez vous connecter afin d'ajouter du contenu dans votre panier<br><a href='compte.php'>Me connecter</a></h4></center>";
     }
+    else if(isset($_GET['validerPanier'])){
+        $request = $bdd->query("select * from Clients, Produits, Commandes, LignesCommandes where Produits.idProduit = LignesCommandes.idProduit and Commandes.idCommande = LignesCommandes.idCommande and Clients.email = Commandes.email and Commandes.email='".$_SESSION['email']."' and etat='Panier'");
+        $prbStock = false;
+        while($donnees = $request->fetch()){
+            if($donnees['stock'] < $donnees['quantite']){
+                $prbStock = true;
+            }
+        }
+        if($prbStock){
+            header("location: panier.php?error=stock");
+        }
+        else if(isset($_POST['ville'])){
+            if(empty($_POST['complement'])) {
+                $adresse = $_POST['numRue'] . " " . ucwords($_POST['rue']) . " " . ucwords($_POST['ville']) . " " . $_POST['CP'];
+            }
+            else{
+                $adresse = $_POST['numRue'] . " " . ucwords($_POST['complement']) . " " . ucwords($_POST['rue']) . " " . ucwords($_POST['ville']) . " " . $_POST['CP'];
+            }
+            $bdd->exec("update Clients set ville='".ucwords($_POST['ville'])."' where email='".$_SESSION['email']."'");
+            $bdd->exec("update Clients set adresse='".$adresse."' where email='".$_SESSION['email']."'");
+            $bdd->exec("update Clients set telephone=".$_POST['tel']." where email='".$_SESSION['email']."'");
+            header("location: panier.php?validerPanier");
+        }
+        $request = $bdd->query("select * from Clients, Commandes where Clients.email = Commandes.email and Clients.email='".$_SESSION['email']."' and etat='Panier'");
+        $donnees = $request->fetch();
+        if(empty($donnees['ville'])){
+            ?>
+            <h4 style="margin-top: 20px">Nous avons besoin de quelques informations supplémentaires :</h4><br>
+            <form action="panier.php?validerPanier" method="POST">
+                <input type="number" name="numRue" placeholder="N° rue" class="panier" required>
+                <input type="text" name="complement" placeholder="Bis, Ter, ..." class="panier">
+                <input type="text" name="rue" placeholder="Rue" class="panier" required>
+                <input type="text" name="ville" placeholder="Ville" class="panier" required>
+                <input type="number" name="CP" placeholder="Code Postal" class="panier" required>
+                <br><br>
+                <input type="tel" name="tel" placeholder="Tel" class="panier" required><br><br>
+                <input type="submit" class="panier">
+            </form>
+            <?php
+        }
+        else{
+            $request = $bdd->query("select SUM(montant) as total from Commandes, LignesCommandes where Commandes.idCommande = lignesCommandes.idCommande and Commandes.email='".$_SESSION['email']."' and etat='Panier'")->fetch();
+            $total = $request['total'];
+
+            ?>
+            <div style="width: 40%">
+                <h4>Montant : <?= $total ?> EUR</h4>
+                <div id="paypal-button-container"></div>
+                <script src="https://www.paypal.com/sdk/js?client-id=AZ0vflfASgal9-8_8PQh029iFgfZecNyYImBso6fu5Qx5I_qcdUVYm6wjt07umoKI_9Sk0aegm1Qk66g"></script>
+                <script src="paypal.js"></script>
+            </div>
+            <?php
+        }
+    }
     else{
         ?>
-        <a href="commande.php" class="button right" style="font-size: 18px">Commandes en cours</a>
+        <a href="#" class="button right" style="font-size: 18px; margin-top: -30px">Commandes en cours</a>
         <h3 class="underline" style="margin-top: 30px">Votre panier actuel</h3>
+        <a href="panier.php?validerPanier" class="button right" style="font-size: 18px; margin-top: 30px">Valider mon Panier</a>
         <?php
 
         if($id = $bdd->query("select idCommande, email, etat from Commandes where email='".$_SESSION['email']."' and etat='Panier'")->fetch()){
@@ -82,5 +137,6 @@ if(isset($_GET['suppProd'])){
         }
     }
 ?>
+
 </body>
 </html>
